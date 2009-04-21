fabc27288edcacd369c425ac606a8aeddf1d1c4e
Josh Dechant <josh@smarterwebdev.com>
2009-04-21 20:46:57 +0000

    Added <motif:script src="..." /> and <motif:style src="..." /> tags
    git-svn-id: http://subversion.assembla.com/svn/phpmotif/trunk@3 0fdf18b5-fe09-4a94-8c34-9a0b2b97716d

---
+++ Library/Motif/Tag/Compiler/Script.php
@@ -0,0 +1,61 @@
+<?php
+/* $Id$ */
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+
+/**
+ * Compile <motif:script src="relative/path/to/script.js" />
+ */
+class Motif_Tag_Compiler_Script extends Motif_Tag_Compiler_Abstract
+{
+
+    /**
+     * @var string Tag's name
+     */
+    protected $_tagName = 'script';
+
+    /**
+     * @var boolean Tag has pairs? (opening and closing)
+     */
+    protected $_hasTagPairs = false;
+
+    /**
+     * Declare attributes for this tag
+     *
+     * @return void
+     */
+    protected function _declareAttributes()
+    {
+        $this->_attributes = array(
+            'src' => new Motif_Tag_Attribute_Required(self::MATCH_WILDCARD),
+        );
+    }
+
+    /**
+     * Compile tag matches to native PHP code
+     */
+    protected function _compileMatches()
+    {
+        foreach ($this->_tagMatches as $match)
+        {
+            $src = $this->getAttribute('src');
+
+            $code = '' .
+                '\');' . NL .
+                'ob_start();' . NL .
+                "if (!isset(\$___vars['motif']['script']))" . NL .
+                '{' . NL .
+                    "\$___vars['motif']['script'] = '';" . NL .
+                '}' . NL .
+                "include(\$___engine->getTemplate()->includeTemplate('$src'));" . NL .
+                "\$___vars['motif']['script'] .= ob_get_clean() . NL;" . NL .
+                'echo(\'';
+
+            /**
+             * Do replacement
+             */
+            $this->_replaceCode($code);
+        }
+    }
+
+}
---
+++ Library/Motif/Tag/Compiler/Style.php
@@ -0,0 +1,61 @@
+<?php
+/* $Id$ */
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+
+/**
+ * Compile <motif:style src="relative/path/to/styles.css" />
+ */
+class Motif_Tag_Compiler_Style extends Motif_Tag_Compiler_Abstract
+{
+
+    /**
+     * @var string Tag's name
+     */
+    protected $_tagName = 'style';
+
+    /**
+     * @var boolean Tag has pairs? (opening and closing)
+     */
+    protected $_hasTagPairs = false;
+
+    /**
+     * Declare attributes for this tag
+     *
+     * @return void
+     */
+    protected function _declareAttributes()
+    {
+        $this->_attributes = array(
+            'src' => new Motif_Tag_Attribute_Required(self::MATCH_WILDCARD),
+        );
+    }
+
+    /**
+     * Compile tag matches to native PHP code
+     */
+    protected function _compileMatches()
+    {
+        foreach ($this->_tagMatches as $match)
+        {
+            $src = $this->getAttribute('src');
+
+            $code = '' .
+                '\');' . NL .
+                'ob_start();' . NL .
+                "if (!isset(\$___vars['motif']['style']))" . NL .
+                '{' . NL .
+                    "\$___vars['motif']['style'] = '';" . NL .
+                '}' . NL .
+                "include(\$___engine->getTemplate()->includeTemplate('$src'));" . NL .
+                "\$___vars['motif']['style'] .= ob_get_clean() . NL;" . NL .
+                'echo(\'';
+
+            /**
+             * Do replacement
+             */
+            $this->_replaceCode($code);
+        }
+    }
+
+}
