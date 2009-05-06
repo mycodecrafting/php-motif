3c71193017a8e8a4205f9eb507b0af4d82f90719
Josh Dechant <josh@smarterwebdev.com>
2009-05-06 01:27:15 +0000

    Add <motif:form:hidden> tag support
    git-svn-id: http://subversion.assembla.com/svn/phpmotif/trunk@5 0fdf18b5-fe09-4a94-8c34-9a0b2b97716d

---
+++ Library/Motif/Tag/Compiler/Form/Hidden.php
@@ -0,0 +1,65 @@
+<?php
+/* $Id$ */
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+
+/**
+ * Compile <motif:form:hidden var="dealTitle" />
+ */
+class Motif_Tag_Compiler_Form_Hidden extends Motif_Tag_Compiler_Abstract
+{
+
+    /**
+     * @var string Tag's name
+     */
+    protected $_tagName = 'form:hidden';
+
+    /**
+     * @var boolean Tag has pairs? (opening and closing)
+     */
+    protected $_hasTagPairs = false;
+
+    /**
+     * Declare attributes for this tag
+     *
+     * @return void
+     */
+    protected function _declareAttributes()
+    {
+        $this->_attributes = array(
+            'var'   => new Motif_Tag_Attribute_Required(self::MATCH_VAR),
+            'value' => new Motif_Tag_Attribute(self::MATCH_WILDCARD),
+        );
+    }
+
+    /**
+     * Compile tag matches to native PHP code
+     */
+    protected function _compileMatches()
+    {
+        foreach ($this->_tagMatches as $match)
+        {
+            $inputName = $this->getAttribute('var');
+            $inputValue = $this->getAttribute('value');
+            $inputAttrs = $this->_getAttributeString($exclude = 'var', 'value');
+
+            /**
+             * Do replacement
+             */
+            $this->_replaceCode(sprintf(
+                '<motif:choose>' . NL .
+                    '<motif:when var="%1$s">' . NL .
+                        '<input type="hidden" name="%1$s" id="%1$s" value="{%1$s}"%3$s />' . NL .
+                    '</motif:when>' . NL .
+                    '<motif:otherwise>' .
+                        '<input type="hidden" name="%1$s" id="%1$s" value="%2$s"%3$s />' . NL .
+                    '</motif:otherwise>' . NL .
+                '</motif:choose>',
+                $inputName,
+                $inputValue,
+                $inputAttrs
+            ));
+        }
+    }
+
+}
